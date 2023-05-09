906d7cdf33def7637418798a384b109e990ce448
Bailey Spell <baspell@microsoft.com>
2023-05-09 16:02:30 -0700

    this works

--- agent/agent.py
+++ agent/agent.py
@@ -1,16 +1,61 @@
 import os
 
-from langchain.agents import AgentType, initialize_agent, load_tools
-from langchain.tools import ShellTool
-from langchain.llms import OpenAI
 from dotenv import load_dotenv
+from langchain.agents import AgentType, initialize_agent, load_tools
+from langchain.chains import LLMChain
+from langchain.llms import OpenAI
+from langchain.prompts import load_prompt
+from langchain.tools import ShellTool
+from langchain.agents import Tool, AgentExecutor, LLMSingleActionAgent, AgentOutputParser
+from langchain.prompts import BaseChatPromptTemplate
+from langchain import SerpAPIWrapper, LLMChain
+from langchain.chat_models import ChatOpenAI
+from typing import List, Union
+from langchain.schema import AgentAction, AgentFinish, HumanMessage
+import re
 
 load_dotenv()
 
-llm = OpenAI(temperature=0, openai_api_key=os.environ["OPENAI_API_KEY"])
+tools = []
+# llm = OpenAI(temperature=0, openai_api_key=os.environ["OPENAI_API_KEY"])
+llm = ChatOpenAI(model_name="gpt-4", temperature=0, openai_api_key=os.getenv("OPENAI_API_KEY"))
 
-tools = load_tools(["serpapi", "llm-math"], llm=llm)
+# Set up a prompt template
+class CustomPromptTemplate(BaseChatPromptTemplate):
+    # The template to use
+    template: str
+    # The list of tools available
+    tools: List[Tool]
+    
+    def format_messages(self, **kwargs) -> str:
+        # Get the intermediate steps (AgentAction, Observation tuples)
+        # Format them in a particular way
+        intermediate_steps = kwargs.pop("intermediate_steps")
+        thoughts = ""
+        for action, observation in intermediate_steps:
+            thoughts += action.log
+            thoughts += f"\nObservation: {observation}\nThought: "
+        # Set the agent_scratchpad variable to that value
+        kwargs["agent_scratchpad"] = thoughts
+        # Create a tools variable from the list of tools provided
+        kwargs["tools"] = "\n".join([f"{tool.name}: {tool.description}" for tool in self.tools])
+        # Create a list of tool names for the tools provided
+        kwargs["tool_names"] = ", ".join([tool.name for tool in self.tools])
+        formatted = self.template.format(**kwargs)
+        return [HumanMessage(content=formatted)]
 
-agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+init = LLMChain(
+    llm=llm,
+    prompt=load_prompt("init_prompt.yaml"),
+)
 
-agent.run("Who is Leo DiCaprio's girlfriend? What is her current age raised to the 0.43 power?")
+platform = init.run("I want to deploy a react app with Azure")
+
+chain = LLMChain(
+    llm=llm,
+    prompt=load_prompt("platform_prompt.yaml"),
+)
+
+steps = chain.run({"platform": platform, "build_folder": "../sample-app/dist"})
+
+print('output', steps)
