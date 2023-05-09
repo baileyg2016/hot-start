a0bfa835c3d29d6732ed4bf70ef2e833b32e2157
Bailey Spell <baspell@microsoft.com>
2023-05-09 10:52:42 -0700

    got everything working locally

---
+++ sample-app/webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: './dist',
+    },
+    compress: true,
+    historyApiFallback: true,
+  },
+};
