bb6056ab2da72d700c9c246c559d35410068214e
Bailey Spell <baspell@microsoft.com>
2023-05-11 18:09:50 -0700

    trying to teset things out

--- agent/executor.py
+++ agent/executor.py
@@ -2,22 +2,45 @@ import json
 import platform
 import secrets
 
-from langchain.chains import LLMChain
+import faiss
+from langchain.chains import LLMChain, ConversationChain
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts import load_prompt
-from steps import Step, load_steps
-from langchain.vectorstores import FAISS
+from langchain.docstore import InMemoryDocstore
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import (ConversationBufferMemory,
+                              VectorStoreRetrieverMemory)
+from langchain.prompts import load_prompt
+from langchain.vectorstores import FAISS
+from steps import Step, load_steps
 
 class Executor:
     def __init__(self):
+        embedding_size = 1536 # Dimensions of the OpenAIEmbeddings
+        index = faiss.IndexFlatL2(embedding_size)
+        embedding_fn = OpenAIEmbeddings().embed_query
+        vectorstore = FAISS(embedding_fn, index, InMemoryDocstore({}), {})
+
+        # In actual usage, you would set `k` to be a higher value, but we use k=1 to show that
+        # the vector lookup still returns the semantically relevant information
+        retriever = vectorstore.as_retriever(search_kwargs=dict(k=1))
+        self.memory = VectorStoreRetrieverMemory(retriever=retriever)
+
         llm = ChatOpenAI(model_name="gpt-4", temperature=0)
         self.chain = LLMChain(
             llm=llm,
             prompt=load_prompt("prompts/platform_prompt.yaml"),
-            memory=ConversationBufferMemory(memory_key="chat_history", return_messages=True),
+            memory=self.memory,
+            verbose=True
         )
+
+        # doing this because I am too lazy to copy and paste the code
+        self.buffer = ConversationChain(
+            llm=llm,
+            prompt=load_prompt("prompts/platform_prompt.yaml"),
+            memory=self.memory,
+            verbose=True
+        )
+
         self.cached_outputs = {}
         self.os = platform.system()
 
@@ -26,7 +49,8 @@ class Executor:
         self.vector_store = FAISS(embeddings=OpenAIEmbeddings())
     
     def __call__(self, platform):
-        output = self.chain.run({"platform": platform, "os": self.os, "build_folder": "../sample-app/dist", "nonce": secrets.token_hex(8)})
+        # need to figure out how to get the memory
+        output = self.chain.run({"platform": platform, "os": self.os, "build_folder": "../sample-app/dist", "nonce": secrets.token_hex(8), "history":  })
         json_steps = json.loads(output)# ["steps"]
         steps = load_steps(json_steps)
         
@@ -34,4 +58,6 @@ class Executor:
             json.dump(json_steps, f, indent=4)
 
         for step in steps:
-            step.exe(platform)
+            success, output = step.exe(platform)
+            if success:
+                self.memory.save_context({ "input": step.command } , { "output": output })
--- agent/scratchpad.py
+++ agent/scratchpad.py
@@ -8,6 +8,9 @@ from langchain.llms import OpenAI
 from langchain.memory import VectorStoreRetrieverMemory
 from langchain.prompts import PromptTemplate
 from langchain.vectorstores import FAISS
+from dotenv import load_dotenv
+
+load_dotenv()
 
 embedding_size = 1536 # Dimensions of the OpenAIEmbeddings
 index = faiss.IndexFlatL2(embedding_size)
@@ -18,6 +21,10 @@ vectorstore = FAISS(embedding_fn, index, InMemoryDocstore({}), {})
 # the vector lookup still returns the semantically relevant information
 retriever = vectorstore.as_retriever(search_kwargs=dict(k=1))
 memory = VectorStoreRetrieverMemory(retriever=retriever)
+
+memory.save_context({"input": "My favorite food is pizza"}, {"output": "thats good to know"})
+memory.save_context({"input": "My favorite sport is soccer"}, {"output": "..."})
+memory.save_context({"input": "I don't the Celtics"}, {"output": "ok"})
 
 llm = OpenAI(temperature=0) # Can be any valid LLM
 _DEFAULT_TEMPLATE = """The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
@@ -40,4 +47,6 @@ conversation_with_summary = ConversationChain(
     memory=memory,
     verbose=True
 )
-conversation_with_summary.predict(input="Hi, my name is Perry, what's up?")
+print(conversation_with_summary.predict(input="Hi, my name is Perry, what's up?"))
+
+# print(conversation_with_summary.)
